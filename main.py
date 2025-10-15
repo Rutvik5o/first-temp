8ec99bf7ee4ca808b8ea5e4eb8cb9cf6e98aab7a
Rutvik5o <prajapatirutvik987@gmail.com>
2025-10-15 18:39:27 +0530

    Mode Python Code

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import os
 
-print(os.listdir())
+
+print("Here is the listdir->",os.listdir())
+
+print("Here is current working directory->",os.getcwd())
+
+print("pritining *")
+
+n=int(input("enter the rnage"))
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(i,j)
